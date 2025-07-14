b767c4ad71f0f9cb922d485c44d8c5dd38c001d5
NayanRane <nayanrane2001@gmail.com>
2025-07-14 18:17:28 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,1 +1,10 @@
+function Header ({username,isLoggedIn,tooglebtn}){
+    return (
+        <>
+        <h1>{isLoggedIn ? `Welcome,${username}`:`Please Login`}</h1>
+        <button onClick={tooglebtn} className="btn btn-dark">{isLoggedIn ? `Logout`:`Login`}</button>
+        </>
+    );
+}
 
+export default Header;
