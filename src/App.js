847a3e29e242a51078d1bf4aafaa8a8f1a7b74ff
NayanRane <nayanrane2001@gmail.com>
2025-07-14 16:37:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+          <div className="d-flex justify-content-center align-items-center vh-100">
+              <div className="card" style={{ width: '30rem', border: '2px solid red' }}>
+                  <div className="card-body">
+                      <input type="text" className="form-control" id="user" placeholder="Enter your name" />
+                      <div className="row mt-5">
+                          <button className="btn btn-dark">Login</button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </>
+  )
+
 }
 
 export default App;
