2b21c754ea50d684d1f7dd2113be0bb70619ea2b
NayanRane <nayanrane2001@gmail.com>
2025-07-15 17:58:54 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./Components/Header";
 import UserList from "./Components/UserList";
+import ToDoApp from "./Components/ToDoApp";
 import { useState } from "react";
 
 function App() {  
@@ -11,13 +12,14 @@ function App() {
  }
   return (
       <>
-          <div className="d-flex justify-content-center align-items-center vh-100">
-              <div className="card" style={{ width: '30rem', border: '2px solid red' }}>
+          <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+              <div className="card d-none" style={{ width: '30rem', border: '2px solid red' }}>
                   <div className="card-body">
                     <Header username = {username} isLoggedIn= {isLoggedIn} tooglebtn = {tooglebtn} />
                     {isLoggedIn && <UserList users={users} />}
                   </div>
               </div>
+              <ToDoApp/>
           </div>
       </>
   )
